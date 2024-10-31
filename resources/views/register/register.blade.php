564c28b405c076282320eb4ac7b5672a666fc669
rizkypratamayudha <masrizki87@gmail.com>
2024-10-31 08:30:04 +0700

    reg

--- resources/views/register/register.blade.php
+++ resources/views/register/register.blade.php
@@ -13,9 +13,6 @@
     <link rel="stylesheet" href="adminlte//plugins/fontawesome-free/css/all.min.css">
     <!-- icheck bootstrap -->
     <link rel="stylesheet" href="adminlte/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-
-    <link rel="stylesheet" href="{{ asset('adminlte/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css') }}">
-
     <!-- Theme style -->
     <link rel="stylesheet" href="adminlte/dist/css/adminlte.min.css">
 </head>
@@ -27,8 +24,8 @@
                 <a href="../../index2.html" class="h1">Register</a>
             </div>
             <div class="card-body">
-                <p class="login-box-msg">Registrasi  akun baru</p>
-                <form action="{{url('register')}}" method="POST" id="form-register">
+                <p class="login-box-msg">Registrasi akun baru</p>
+                <form action="{{ url('register') }}" method="POST" id="form-register">
                     @csrf
                     <div class="input-group mb-3">
                         <select class="form-control" id="level_id" name="level_id" required>
@@ -42,9 +39,6 @@
                                 <span class="fas fa-level-down-alt"></span>
                             </div>
                         </div>
-                        @error('level_id')
-                            <small class="form-text text-danger">{{ $message }}</small>
-                        @enderror
                     </div>
                     <div class="input-group mb-3">
                         <input id="nama" name="nama" type="text" class="form-control" placeholder="Full Name" required>
@@ -70,24 +64,60 @@
                             </div>
                         </div>
                     </div>
+                    <div id="additional-fields">
+                        <div class="input-group mb-3" id="email-field" style="display: none;">
+                            <input id="email" name="email" type="email" class="form-control"
+                                placeholder="Email">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-envelope"></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="input-group mb-3" id="no-hp-field" style="display: none;">
+                            <input id="no_hp" name="no_hp" type="text" class="form-control"
+                                placeholder="No HP">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-phone"></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="input-group mb-3" id="prodi-field" style="display: none;">
+                            <input id="prodi" name="prodi" type="text" class="form-control"
+                                placeholder="Prodi">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-university"></span>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="input-group mb-3" id="angkatan-field" style="display: none;">
+                            <input id="angkatan" name="angkatan" type="text" class="form-control"
+                                placeholder="Angkatan">
+                            <div class="input-group-append">
+                                <div class="input-group-text">
+                                    <span class="fas fa-calendar-alt"></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                     <div class="row">
                         <div class="col-8">
-                            <a href="{{url('login')}}" class="text-center">Sudah punya akun?</a>
+                            <a href="{{ url('login') }}" class="text-center">Sudah punya akun?</a>
                         </div>
-                        <!-- /.col -->
                         <div class="col-4">
-                            <button type="submit" class="btn btn-primary btn-block">Register</button >
+                            <button type="submit" class="btn btn-primary btn-block">Register</button>
                         </div>
-                        <!-- /.col -->
                     </div>
                 </form>
-
-
             </div>
-            <!-- /.form-box -->
-        </div><!-- /.card -->
+        </div>
     </div>
-    <!-- /.register-box -->
 
     <!-- jQuery -->
     <script src="adminlte/plugins/jquery/jquery.min.js"></script>
@@ -96,11 +126,22 @@
     <!-- AdminLTE App -->
     <script src="adminlte/dist/js/adminlte.min.js"></script>
 
-    <script src="{{ asset('adminlte/plugins/jquery-validation/jquery.validate.min.js') }}"></script>
-
-    <script src="{{ asset('adminlte/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
-
     <script>
+        $(document).ready(function() {
+            // Show/hide fields based on role selection
+            $('#level_id').change(function() {
+                var selectedRole = $(this).val();
+
+                $('#email-field, #no-hp-field, #prodi-field, #angkatan-field').hide();
+
+                if (selectedRole == '3') { // Mahasiswa
+                    $('#email-field, #no-hp-field, #prodi-field, #angkatan-field').show();
+                } else if (selectedRole == '2' || selectedRole == '4') { // Dosen or Kaprodi
+                    $('#email-field, #no-hp-field').show();
+                }
+            });
+        });
+
         $(document).ready(function() {
             $("#form-register").validate({
                 rules: {
@@ -133,7 +174,7 @@
                             if (response.status) { // jika sukses
                                 Swal.fire({
                                     icon: 'success',
-                                    title: 'Register Berhasil',
+                                    title: 'Mohon Tunggu Verifikasi\nDari Admin',
                                     text: response.message,
                                 }).then(function() {
                                     window.location = response.redirect;
