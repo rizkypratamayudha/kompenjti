7f16e8cea38d4e6f0bcae09f63fd4765886941a8
rizkypratamayudha <masrizki87@gmail.com>
2024-11-09 14:55:34 +0700

    token

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -6,10 +6,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
     use HasFactory;
+
+    public function getJWTIdentifier()
+    {
+            return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
     protected $fillable = [
