b6f4a6e88fd05c2298230f6b1afb70e2963ce8da
Solikhin <solikhinkhin645@gmail.com>
2024-11-12 20:28:44 +0700

    edit

--- resources/views/user/edit_ajax.blade.php
+++ resources/views/user/edit_ajax.blade.php
@@ -59,7 +59,7 @@
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" data-dismiss="modal" class="btn btnwarning">Batal</button>
+                    <button type="button" data-dismiss="modal" class="btn btn-warning">Batal</button>
                     <button type="submit" class="btn btn-primary">Simpan</button>
                 </div>
             </div>
