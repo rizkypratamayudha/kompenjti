1ae3ef32d0d13fbf5a8738859a91ae856a5ae963
rizkypratamayudha <masrizki87@gmail.com>
2024-11-16 18:12:26 +0700

    persyaratan model

---
+++ app/Models/PersyaratanModel.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class PersyaratanModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'persyaratan';
+    protected $primaryKey = 'persyaratan_id';
+    protected $fillable = ['persyaratan_id','detail_pekerjaan_id','persyaratan_nama'];
+
+    public function detail_pekerjaan():BelongsTo{
+        return $this->belongsTo(PekerjaanModel::class,'detail_pekerjaan_id','detail_pekerjaan_id');
+    }
+}
