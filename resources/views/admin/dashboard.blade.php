313be1996f16dbc3176d7c4b3acd87a4263d2569
Solikhin <solikhinkhin645@gmail.com>
2024-12-08 18:23:24 +0700

    pie chart dipisah

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -64,7 +64,7 @@
         <div class="card shadow-sm border-0 rounded-lg">
             <div class="card-header bg-warning text-white d-flex justify-content-between align-items-center">
                 <h4 class="card-title mb-0">
-                    <i class="bi bi-pie-chart-fill me-3 mr-3 ml-3"></i>Statistik Kompen & Level Pengguna
+                    <i class="bi bi-pie-chart-fill me-3 mr-3 ml-3"></i>Level Pengguna Dan Statistik Kompen
                 </h4>
             </div>
             <div class="card-body">
@@ -72,14 +72,14 @@
                     <!-- Distribusi Level Pengguna Chart -->
                     <div class="col-md-6 mb-4 d-flex justify-content-center">
                         <div class="py-3">
-                            <canvas id="levelPieChart" style="width: 400px; height: 400px; max-width: 100%;"></canvas>
+                            <canvas id="levelPieChart" style="width: 360px; height: 360px; max-width: 100%;"></canvas>
                         </div>
                     </div>
 
                     <!-- Status Kompen Chart -->
                     <div class="col-md-6 mb-4 d-flex justify-content-center">
                         <div class="py-3">
-                            <canvas id="kompenPieChart" style="width: 400px; height: 400px; max-width: 100%;"></canvas>
+                            <canvas id="kompenPieChart" style="width: 360px; height: 360px; max-width: 100%;"></canvas>
                         </div>
                     </div>
                 </div>
@@ -99,11 +99,11 @@
     new Chart(ctxLevel, {
         type: 'pie',
         data: {
-            labels: chartData.labels, // Use the labels from the controller
+            labels: ['Total Mahasiswa', 'Total Dosen/Tendik', 'Total Kaprodi','Total Pekerjaan'], // Example labels for status
             datasets: [{
-                data: chartData.data, // Use the data from the controller
-                backgroundColor: ['#2196f3', '#4caf50', '#ff9800', '#9e9e9e', '#f44336', '#8bc34a'], // Custom colors
-                hoverBackgroundColor: ['#64b5f6', '#66bb6a', '#ffb74d', '#cfd8dc', '#ef9a9a', '#c5e1a5']
+                data: [{{ $totalMahasiswa }}, {{ $totalDosenTendik }}, {{ $totalKaprodi }},{{ $totalPekerjaan }}], // Use the data from the controller
+                backgroundColor: ['#2196f3', '#4caf50', '#ff9800', '#9e9e9e'], // Custom colors
+                hoverBackgroundColor: ['#64b5f6', '#66bb6a', '#ffb74d', '#cfd8dc']
             }]
         },
         options: {
